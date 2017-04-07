be63b719d48cba150e8b610e4f0cd7b097cfac24
LogoiLab <cbaxter@mail.umw.edu>
2017-04-07 18:42:36 -0400

    commit of the day, incomplete

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ use clap::{Arg,App};
 
 use hyper::Client;
 use hyper::client::Response;
+use hyper::header::Headers;
+use hyper::header::Header;
+use hyper::mime::*;
 
 use ipaddress::IPAddress;
 
 fn v0(ip_address: &IPAddress, detect_type: String) {
-    if detect_type == String::from("client") {
+    if detect_type == String::from("server") {
         unimplemented!();
     } else {
         unimplemented!();
@@ -21,17 +24,40 @@ fn v0(ip_address: &IPAddress, detect_type: String) {
 }
 
 fn v1(ip_address: &IPAddress, detect_type: String) {
-    if detect_type == String::from("client") {
+    if detect_type == String::from("server") {
         let high: &IPAddress = &ip_address.first();
         let low: &IPAddress = &ip_address.last();
         let client = Client::new();
 
         println!("[verbose]: Starting client.");
-        for i in high.to_string().split(".").collect::<Vec<&str>>().last().unwrap().parse::<i32>().unwrap() .. low.to_string().split(".").collect::<Vec<&str>>().last().unwrap().parse::<i32>().unwrap() {
-            let ip: String = low.to_string() + i.to_string().as_str();
+        println!("{}", &high.to_string().split(".").collect::<Vec<&str>>().last().unwrap().to_string());
+        for i in high.to_string().split(".").collect::<Vec<&str>>().last().unwrap().to_string().split_at_slash()[1].parse::<i32>().unwrap() .. low.to_string().split(".").collect::<Vec<&str>>().last().unwrap().parse::<i32>().unwrap() {
+            let mut low_parts: Vec<String> = low.to_string().split(".").map(|p| p.to_string()).collect();
+            let mut ip: String = String::new();
+            for part in 0 .. (low_parts.len() + 1) {
+                if part != low_parts.len() {
+                    ip.push_str(low_parts.remove(0).as_str());
+                    ip.push('.');
+                } else {
+                    ip.push_str(i.to_string().as_str());
+                }
+            }
+
             println!("[verbose]: Sending GET request for IP address: {}", &ip);
-            let response = client.get("http://google.com/").send().unwrap();
-            println!("{:?}", response);
+            let response = client.get(ip.as_str()).send();
+            let head_map: &Vec<String> = &response.unwrap().headers.iter().map(|h| {println!("[verbose]: Header part: {}", h.value_string()); h.value_string()}).collect();
+            let mut variances: usize = 0;
+            for h in head_map {
+                if h.contains("HTTP/1.0 200 OK") || h.contains("Server: Microsoft-IIS/7.5") || h.contains("Date: "){
+                    println!("[verbose]: Similarity found.");
+                    variances = variances + 1;
+                }
+            }
+            let mut head_match: bool;
+            if variances == 3 {
+                println!("[verbose]: Header match found for: {}", &ip);
+                head_match = true;
+            }
         }
     } else {
         unimplemented!();
@@ -65,14 +91,6 @@ fn main() {
                     )
                     .get_matches();
     let client = Client::new();
-    let mut response = client.get("http://google.com/").send().unwrap();
-
-    let body = response.bytes();
-    let mut temp: Vec<u8> = vec!();
-    for byte in body {
-        temp.push(byte.unwrap());
-    }
-    println!("{:?}", String::from_utf8(temp));
 
     let ip_address: Result<IPAddress, String> = match IPAddress::is_valid(matches.value_of("ipaddress").unwrap()) {
         true => IPAddress::parse(matches.value_of("ipaddress").unwrap()),
